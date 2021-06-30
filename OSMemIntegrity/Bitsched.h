a1a0d473c5a370532ee029136f7d93889beb3230
Alon Hillel-Tuch <ah5647@nyu.edu>
2021-06-30 17:48:34 -0400

    Random BIT GEN done

--- OSMemIntegrity/Bitsched.h
+++ OSMemIntegrity/Bitsched.h
@@ -1,10 +1,9 @@
 #pragma once
 
 #include<iostream>
-#include<string>
+//#include<string>
 
-using namespace std;
-
+//using namespace std;
 
 struct WordBit;
 struct MemAlloc;
@@ -26,6 +25,7 @@ struct WordBit
 class PriorityScheduler
 {
 private:
+	//Tables are indexed and can follow any array based data structure
 	MemAlloc** priority;
 	MemAlloc** nonpriority;
 
@@ -40,19 +40,17 @@ private:
 public:
 
 	PriorityScheduler(int priority_list_init = 0, int non_priority_list_init = 0, MemAlloc** priority_ptr = nullptr, MemAlloc** non_priority_ptr = nullptr) :nonpriority_physical_size(non_priority_list_init), non_priority_logical_size(non_priority_list_init), priority_physical_size(priority_list_init), priority_logical_size(priority_list_init), priority(priority_ptr), nonpriority(non_priority_ptr) {}
-
+	~PriorityScheduler();
 	//getters
 	int get_priority_size() { return priority_logical_size; }
 	int get_non_priority_size() { return non_priority_logical_size; }
 
 	//setters
-	MemAlloc* create_entry(string word, int priority);
+	MemAlloc* create_entry(std::string word, int priority);
 	void add_to_table(MemAlloc* temp_alloc);
-
 
 	//helpers
 	void add_alloc(MemAlloc**& table, MemAlloc* entry, int& logical_size, int& physical_size);
-
 
 	//test functions
 	void display_memalloc(MemAlloc* temp_ptr);
@@ -60,17 +58,29 @@ public:
 	void display_non_priority_list() { display_list(1); }
 	void display_list(int8_t list);
 
-
 	void test_run()
 	{
-
 		display_memalloc(nonpriority[0]);
-
 	}
 };
 
+inline PriorityScheduler::~PriorityScheduler()
+{
+	//release structs
+	if (priority != nullptr) {
+		delete[] * priority;
+	}
 
-MemAlloc* PriorityScheduler::create_entry(string word, int priority)
+	if(nonpriority!=nullptr){
+
+	delete[] * nonpriority;
+	}
+	
+	priority = nullptr;
+	nonpriority = nullptr;
+}
+
+MemAlloc* PriorityScheduler::create_entry(std::string word, int priority)
 {
 	WordBit* word_array = nullptr;
 	int* parity_ptr = nullptr;
@@ -101,7 +111,6 @@ MemAlloc* PriorityScheduler::create_entry(string word, int priority)
 		}
 	}
 
-
 	if (zero_counter % 2 == 0)
 	{
 		parity_ptr = new int(0);
@@ -118,23 +127,16 @@ MemAlloc* PriorityScheduler::create_entry(string word, int priority)
 
 void PriorityScheduler::add_to_table(MemAlloc* temp_alloc)
 {
-
 	if (*(temp_alloc->priority) == 0) {
 		//priority
 
 		add_alloc(priority, temp_alloc, priority_logical_size, priority_physical_size);
-
 	}
 	else
 	{
 		//not priority
 		add_alloc(nonpriority, temp_alloc, non_priority_logical_size, nonpriority_physical_size);
-
 	}
-
-
-
-
 }
 
 void PriorityScheduler::add_alloc(MemAlloc**& table, MemAlloc* entry, int& logical_size, int& physical_size)
@@ -149,88 +151,68 @@ void PriorityScheduler::add_alloc(MemAlloc**& table, MemAlloc* entry, int& logic
 	}
 	else if (logical_size < physical_size)
 	{
-
 		table[logical_size] = entry;
 		logical_size++;
-
-
 	}
 	else if (logical_size == physical_size)
 	{
 		int new_physical_size = physical_size * DYNAMIC_INCREASE_FACTOR;
-		MemAlloc* temp = new MemAlloc[new_physical_size];
+		MemAlloc** temp = new MemAlloc*[new_physical_size];
 
-		for (unsigned int i = 0; i < logical_size; i++)
+		for (size_t i = 0; i < logical_size; i++)
 		{
-			temp[i] = *table[i];
+			temp[i] = table[i];
 		}
 
-		delete[] * table;
-		*table = temp;
+		delete[] table;
+		table = temp;
 
 		physical_size = new_physical_size;
 
 		table[logical_size] = entry;
 		logical_size++;
-
 	}
-
-
 }
-
-
-
-
-
 
 void PriorityScheduler::display_memalloc(MemAlloc* temp_ptr)
 {
-
 	//printf("BIT: %d\n", new_task->word[0].bit_val);
 	if (temp_ptr != nullptr) {
-
 		printf("Word: ");
 		for (unsigned int i = 0; i < *(temp_ptr->word_size); i++)
 		{
-
 			printf("%d ", temp_ptr->word[i].bit_val);
 		}
 		printf("Parity Bit: %d", *(temp_ptr->parity));
-
 	}
-	cout << endl;
-
-
+	printf("\n");
 }
 
 void PriorityScheduler::display_list(int8_t list)
 {
-
 	MemAlloc** temp_list = nullptr;
 	int list_size = 0;
 
-
-	if (list == 0)
+	if (list == 0 && priority != nullptr)
 	{
 		temp_list = priority;
 		list_size = this->priority_logical_size;
 
+		for (unsigned int i = 0; i < list_size; i++)
+		{
+			display_memalloc(temp_list[i]);
+		}
+		printf("\n");
 	}
-	else if (list == 1)
+	else if (list == 1 && nonpriority != nullptr)
 	{
 		temp_list = nonpriority;
 		list_size = this->non_priority_logical_size;
+
+		for (unsigned int i = 0; i < list_size; i++)
+		{
+			display_memalloc(temp_list[i]);
+		}
+		printf("\n");
 	}
-
-
-
-	for (unsigned int i = 0; i < list_size; i++)
-	{
-
-		display_memalloc(temp_list[i]);
-
-	}
-
-
-
 }
--- OSMemIntegrity/main.cpp
+++ OSMemIntegrity/main.cpp
@@ -1,10 +1,19 @@
 #include<iostream>
+#include <random>
+#include<string>
+#include<vector>
+
+
+
+#include <algorithm>
+#include <sstream>
+#include <iterator>
 
 #include"Bitsched.h"
 
-
 using namespace std;
 
+void random_assignment(PriorityScheduler& schedule, double priority_alloc, int bit_length, long total_entries);
 
 int main()
 {
@@ -12,16 +21,20 @@ int main()
 	
 
 	
-	MemAlloc* new_task = schedule1.create_entry("101011", 1);
-	schedule1.add_to_table(new_task);
-
-
+	/*
+	schedule1.add_to_table(schedule1.create_entry("101011", 1));
 	schedule1.add_to_table(schedule1.create_entry("111111", 1));
-	
 	schedule1.add_to_table(schedule1.create_entry("100001", 1));
-
 	schedule1.add_to_table(schedule1.create_entry("100101", 1));
+	
 
+	schedule1.add_to_table(schedule1.create_entry("101011", 0));
+	schedule1.add_to_table(schedule1.create_entry("111011", 0));
+	schedule1.add_to_table(schedule1.create_entry("101001", 0));
+	schedule1.add_to_table(schedule1.create_entry("101101", 0));
+	*/
+
+	random_assignment(schedule1, 0.5, 6, 20);
 	
 	//schedule1.display_priority_list();
 	schedule1.display_non_priority_list();
@@ -31,3 +44,34 @@ int main()
 }
 
 
+void random_assignment(PriorityScheduler& schedule, double priority_alloc, int bit_length, long total_entries)
+{
+	std::default_random_engine defaultRandomEngine(std::random_device{}());
+	std::uniform_int_distribution<> uniformDistribution(0, 1);
+	std::ostringstream* oss;
+
+
+	vector<char> temp_string;
+
+
+	for (int i = 0; i < total_entries; i++) {
+		oss = new ostringstream;
+		
+		for (int j = 0; j < bit_length; ++j)
+		{
+			temp_string.push_back(static_cast<char>(uniformDistribution(defaultRandomEngine)));
+		}
+
+
+		std::copy(temp_string.begin(), temp_string.end() - 1,std::ostream_iterator<int>(*oss, ""));
+
+		// last element (no delimiter(
+		*oss << temp_string.back();
+
+		schedule.add_to_table(schedule.create_entry(oss->str(), 1));
+
+		delete oss;
+		oss = nullptr;
+		temp_string.clear();
+	}
+}
